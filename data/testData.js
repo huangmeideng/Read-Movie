73b31a742882f06e71c9f69327ebb8146fb968b6
huangmeideng <huangmeideng@outlook.com>
2018-11-28 16:57:15 +0800

    post-list
    文章列表

---
+++ data/testData.js
@@ -0,0 +1,41 @@
+let swiperData = [
+  {
+    id: 1,
+    imgUrl: '../../static/images/1.jpg',
+    httpUrl: 'www.baidu.com'
+  },
+  {
+    id: 2,
+    imgUrl: '../../static/images/2.jpg',
+    httpUrl: 'www.qq.com'
+  }
+];
+
+let articleData = [
+  {
+    id: 1,
+    articleTitle: '小程序地图开发太复杂,地图插件了解一下,更好的开发',
+    articleDate: '2018-11-28',
+    articleSource: '腾讯官方',
+    articleImage: '../../static/images/3.jpg'
+  },
+  {
+    id: 2,
+    articleTitle: '手机秒变公交卡小程序HCE能力全面开放',
+    articleDate: '2018-11-30',
+    articleSource: 'CSDN',
+    articleImage: '../../static/images/4.jpg'
+  },
+  {
+    id: 3,
+    articleTitle: '春节玩一个不一样的小游戏跳一跳',
+    articleDate: '2018-12-01',
+    articleSource: '腾讯官方',
+    articleImage: '../../static/images/3.jpg'
+  }
+];
+
+module.exports = {
+  swiperData,
+  articleData
+}
